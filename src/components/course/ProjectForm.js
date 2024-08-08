c8e88c068ecd0e5401a4588d0aac3c8adb9f6958
Dave Berzack <dave.berzack@nashvillesoftwareschool.com>
2024-08-08 16:36:28 -0400

    added project type radio buttons with state, and sent data for advanced and project info (description and template url)

--- src/components/course/ProjectForm.js
+++ src/components/course/ProjectForm.js
@@ -21,8 +21,13 @@ export const ProjectForm = () => {
         index: 0,
         implementation_url: "",
         active: true,
-        is_group_project: false
+        is_group_project: false,
+        is_advanced_project: false,
+        description: "",
+        template_url: ""
     })
+    const [projectType, setProjectType] = useState("normal");
+
     const history = useHistory()
     const { projectId, bookId } = useParams()
 
@@ -30,10 +35,32 @@ export const ProjectForm = () => {
         getCourses().then(setCourses)
     }, [])
 
+    
+    useEffect(() => {
+        const newProject = {...project};
+        switch (projectType) {
+            case "group": 
+                newProject.is_advanced_project=false;
+                newProject.is_group_project=true;
+                break;
+            case "advanced": 
+                newProject.is_advanced_project=true;
+                newProject.is_group_project=false;
+                break;
+            default: 
+                newProject.is_advanced_project=false;
+                newProject.is_group_project=false;
+        }
+        updateProject(newProject);
+    }, [projectType])
+
     useEffect(() => {
         if (projectId) {
             getProject(projectId).then((project) => {
-                updateProject({ ...project, course: project.course.id, book: project.book.id })
+                updateProject({ ...project, course: project.course.id, book: project.book.id, description: project.info?.description, template_url: project.info?.template_url })
+                if (project.is_advanced_project) setProjectType("advanced");
+                else if (project.is_group_project) setProjectType("group");
+                else setProjectType("normal");
             })
             setMode("edit")
         }
@@ -56,7 +83,12 @@ export const ProjectForm = () => {
     }, [project])
 
     const constructNewProject = () => {
-        fetchIt(`${Settings.apiHost}/projects`, { method: "POST", body: JSON.stringify(project) })
+        const projectData = JSON.stringify(project);
+        if (!project.is_advanced_project) {
+            projectData.description = "";
+            projectData.template_url = "";
+        }
+        fetchIt(`${Settings.apiHost}/projects`, { method: "POST", body: projectData })
             .then(() => history.push(`/books/${project.book}`))
     }
 
@@ -105,14 +137,64 @@ export const ProjectForm = () => {
                 </div>
 
                 <div className="form-group">
-                    <input onChange={updateState}
-                        checked={project.is_group_project}
-                        type="checkbox" required
-                        controltype="boolean"
-                        id="is_group_project"
-                    />
-                    <label htmlFor="is_group_project"> Group project </label>
+                    <div className="radio-option">
+                        <input
+                            name="projectType"
+                            type="radio"
+                            value="normal"
+                            checked={projectType==="normal"}
+                            onChange={ ()=> {setProjectType("normal")}}
+                        />
+                        <label>Normal</label>
+                    </div>
+                    <div className="radio-option">
+                        <input
+                            name="projectType"
+                            type="radio"
+                            value="group"
+                            checked={projectType==="group"}
+                            onChange={ ()=> {setProjectType("group")}}
+                        />
+                        <label>Group</label>
+                    </div>
+                    <div className="radio-option">
+                        <input
+                            name="projectType"
+                            type="radio"
+                            value="advanced"
+                            checked={projectType==="advanced"}
+                            onChange={ ()=> {setProjectType("advanced")}}
+                        />
+                        <label>Advanced</label>
+                    </div>
                 </div>
+                
+                { project.is_advanced_project &&
+                    <>
+                        <div className="form-group">
+                            <label htmlFor="name">
+                                Description
+                            </label>
+                            <input onChange={updateState}
+                                value={project.description}
+                                type="text"
+                                controltype="string"
+                                id="description" className="form-control"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="name">
+                                Template Url
+                            </label>
+                            <input onChange={updateState}
+                                value={project.template_url}
+                                type="url"
+                                controltype="string"
+                                id="template_url" className="form-control"
+                            />
+                        </div>
+                    </>
+                }
 
                 <div className="form-group">
                     <input onChange={updateState}
